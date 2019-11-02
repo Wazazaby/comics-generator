ca3c9f64ae994ef065bba3e6337f919a5abc7a3c
Teddy <megareda1998@gmail.com>
2019-11-02 02:17:09 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,62 @@
+//const firstTitle = document.querySelector('#option-set-1 h2');
+//const typed = new Typed(firstTitle, {strings: [firstTitle.dataset.myText], showCursor: false, typeSpeed: 10});
+const links = document.querySelectorAll('.options .column a');
+
+for(let link of links){
+	const target = document.querySelector(link.hash);
+
+	link.addEventListener('click', event => {
+		event.preventDefault();
+		target.classList.remove('is-hidden');
+		target.scrollIntoView({behavior: 'smooth', block: 'center'});
+
+		const h3h3 = target.querySelector('h2');
+		h3h3.innerHTML = new Typed(h3h3, {strings: [h3h3.dataset.myText], showCursor: false, typeSpeed: 20, startDelay: 100});
+
+		const imagesJson = JSON.parse(event.srcElement.dataset.choiceImage);
+		const imagesArray = imagesJson.images;
+
+		const displayImages = document.getElementById('display-images');
+
+		if(imagesArray && imagesArray.length > 0){
+			for(let image of imagesArray){
+				const imgTag = document.createElement('IMG');
+				imgTag.src = image;
+				imgTag.crossOrigin = 'anonymous';
+
+				setTimeout(() => {
+					if(imgTag.naturalWidth > 3000){
+						imgTag.classList.add('image-is-two');
+					}else{
+						imgTag.classList.add('image-is-one');
+					}
+				}, 100);
+
+				displayImages.appendChild(imgTag);
+			}
+		}
+	});
+}
+
+const saveAs = (uri, filename) => {
+    const link = document.createElement('a');
+
+    if(typeof(link.download) === 'string'){
+      	link.href = uri;
+      	link.download = filename;
+      	document.body.appendChild(link);
+      	link.click();
+      	document.body.removeChild(link);
+    }else{
+      	window.open(uri);
+    }
+}
+
+document.getElementById('save-comic').addEventListener('click', event => {
+	const bd = document.getElementById('display-images');
+
+	html2canvas(bd).then(canvas => {
+    	let test = canvas.toDataURL()
+    	console.log(test)
+	});
+});
