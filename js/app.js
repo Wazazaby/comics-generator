2b6cd340a6e14faf05ea1dc5d18732088d137ac4
Teddy <megareda1998@gmail.com>
2019-11-02 15:00:57 +0100

    Orthographe + js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,39 @@
 const links = document.querySelectorAll('.options .column a');
 
-for(let link of links){
-	const target = document.querySelector(link.hash);
-
-	link.addEventListener('click', event => {
-		event.preventDefault();
-		target.classList.remove('is-hidden');
-		target.scrollIntoView({behavior: 'smooth', block: 'center'});
-
-		const h3h3 = target.querySelector('h2');
-		h3h3.innerHTML = new Typed(h3h3, {strings: [h3h3.dataset.myText], showCursor: false, typeSpeed: 10, startDelay: 100});
-
-		const imagesJson = JSON.parse(event.srcElement.dataset.choiceImage);
-		const imagesArray = imagesJson.images;
-
-		const displayImages = document.getElementById('display-images');
-
-		if(imagesArray && imagesArray.length > 0){
-			for(let image of imagesArray){
-				const imgTag = document.createElement('IMG');
-                imgTag.src = image;
-
-				setTimeout(() => {
-                    if(imgTag.naturalWidth < 2800){
-                        imgTag.classList.add('image-is-one');
-                    }else{
-                        imgTag.classList.add('image-is-two');
-                    }
-				}, 500);
-
-                displayImages.appendChild(imgTag);
-			}
-		}
-	});
+if(links && links.length > 0){
+    for(let link of links){
+        const target = document.querySelector(link.hash);
+    
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            target.classList.remove('is-hidden');
+            target.scrollIntoView({behavior: 'smooth', block: 'center'});
+    
+            const h3h3 = target.querySelector('h2');
+            h3h3.innerHTML = new Typed(h3h3, {strings: [h3h3.dataset.myText], showCursor: false, typeSpeed: 10, startDelay: 100});
+    
+            const imagesJson = JSON.parse(event.srcElement.dataset.choiceImage);
+            const imagesArray = imagesJson.images;
+    
+            const displayImages = document.getElementById('display-images');
+    
+            if(imagesArray && imagesArray.length > 0){
+                for(let image of imagesArray){
+                    const imgTag = document.createElement('IMG');
+                    imgTag.src = image;
+    
+                    setTimeout(() => {
+                        console.log(imgTag.naturalWidth)
+                        if(imgTag.naturalWidth < 2800){
+                            imgTag.classList.add('image-is-one');
+                        }else{
+                            imgTag.classList.add('image-is-two');
+                        }
+                    }, 1000);
+    
+                    displayImages.appendChild(imgTag);
+                }
+            }
+        });
+    }
 }
